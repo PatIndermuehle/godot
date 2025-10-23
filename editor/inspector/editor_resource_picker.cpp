d3e7d544234b07f43d588e63bb9a3942a5978e01
PIndermuehle <pindermuehle@protonmail.com>
2025-10-23 18:32:07 +0200

    Make recursive unique also affect dictionaries

--- editor/inspector/editor_resource_picker.cpp
+++ editor/inspector/editor_resource_picker.cpp
@@ -58,7 +58,7 @@ static bool _has_sub_resources(const Ref<Resource> &p_res) {
 				return true;
 			}
 
-			Variant gotten_value = p_res->getvar(p.name);
+			Variant gotten_value = p_res->get(p.name);
 
 			if (gotten_value.is_array()) {
 				Array gotten_array = gotten_value;
@@ -66,6 +66,21 @@ static bool _has_sub_resources(const Ref<Resource> &p_res) {
 				for (int i = 0; i < gotten_array.size(); i++) {
 					Ref<Resource> item_res = gotten_array[i];
 					if (item_res.is_valid() && !item_res->get_path().is_empty()) {
+						return true;
+					}
+				}
+			}
+
+			if (gotten_value.is_dictionary()) {
+				Dictionary gotten_dictionary = gotten_value;
+
+				for (int i = 0; i < gotten_dictionary.size(); i++) {
+					Ref<Resource> item_key_res = gotten_dictionary.get_key_at_index(i);
+					if (item_key_res.is_valid() && !item_key_res->get_path().is_empty()) {
+						return true;
+					}
+					Ref<Resource> item_value_res = gotten_dictionary.get_valid(gotten_dictionary.get_key_at_index(i));
+					if (item_value_res.is_valid() && !item_value_res->get_path().is_empty()) {
 						return true;
 					}
 				}
@@ -1167,12 +1182,12 @@ void EditorResourcePicker::_gather_resources_to_duplicate(const Ref<Resource> p_
 						if (item_res.is_valid() && !item_res->get_path().is_empty()) {
 
 							TreeItem *child = p_item->create_child();
-							String array_index_append;
-							array_index_append += "[";
-							array_index_append += String::num(i,0).ptr();
-							array_index_append += "]";
+							String array_index_text;
+							array_index_text += "[";
+							array_index_text += String::num(i, 0).ptr();
+							array_index_text += "]";
 
-							_gather_resources_to_duplicate(item_res, child, E.name + array_index_append);
+							_gather_resources_to_duplicate(item_res, child, E.name + array_index_text);
 
 							meta = child->get_metadata(0);
 							// Remember property name.
@@ -1189,11 +1204,73 @@ void EditorResourcePicker::_gather_resources_to_duplicate(const Ref<Resource> p_
 					}
 				}
 			}
+			else if (E.type == Variant::DICTIONARY) {
+				Variant gotten_value = p_resource->getvar(E.name);
+
+				if (gotten_value.is_dictionary()) {
+					Dictionary gotten_dictionary = gotten_value;
+
+					for (int i = 0; i < gotten_dictionary.size(); i++) {
+						Variant gotten_key = gotten_dictionary.get_key_at_index(i);
+						Ref<Resource> item_key_res = gotten_key;
+						Ref<Resource> item_value_res = gotten_dictionary.get_valid(gotten_key);
+						if (item_key_res.is_valid() && !item_key_res->get_path().is_empty()) {
+							TreeItem *child = p_item->create_child();
+							String dictionary_index_text;
+							dictionary_index_text += " ";
+							dictionary_index_text += TTRC("Key");
+							dictionary_index_text += "[";
+							dictionary_index_text += String::num(i, 0).ptr();
+							dictionary_index_text += "]";
+
+							_gather_resources_to_duplicate(item_key_res, child, E.name + dictionary_index_text);
+
+							meta = child->get_metadata(0);
+							// Remember property name.
+							meta.append(E.name);
+							meta.append(Variant::DICTIONARY);
+							meta.append(i);
+							meta.append(true);
+
+							if ((E.usage & PROPERTY_USAGE_NEVER_DUPLICATE)) {
+								// The resource can't be duplicated, but make it appear on the list anyway.
+								child->set_checked(0, false);
+								child->set_editable(0, false);
+							}
+						}
+						if (item_value_res.is_valid() && !item_value_res->get_path().is_empty()) {
+							TreeItem *child = p_item->create_child();
+							String dictionary_index_text;
+							dictionary_index_text += " ";
+							dictionary_index_text += TTRC("Value");
+							dictionary_index_text += "[";
+							dictionary_index_text += String::num(i, 0).ptr();
+							dictionary_index_text += "]";
+
+							_gather_resources_to_duplicate(item_value_res, child, E.name + dictionary_index_text);
+
+							meta = child->get_metadata(0);
+							// Remember property name.
+							meta.append(E.name);
+							meta.append(Variant::DICTIONARY);
+							meta.append(i);
+							meta.append(false);
+
+							if ((E.usage & PROPERTY_USAGE_NEVER_DUPLICATE)) {
+								// The resource can't be duplicated, but make it appear on the list anyway.
+								child->set_checked(0, false);
+								child->set_editable(0, false);
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 }
 
 void EditorResourcePicker::_duplicate_selected_resources() {
+	bool has_resource_changed = false;
 	for (TreeItem *item = duplicate_resources_tree->get_root(); item; item = item->get_next_in_tree()) {
 		if (!item->is_checked(0)) {
 			continue;
@@ -1207,25 +1284,46 @@ void EditorResourcePicker::_duplicate_selected_resources() {
 
 		if (meta.size() == 1) { // Root.
 			edited_resource = unique_resource;
-			_resource_changed();
-		} else if (meta.size() > 2) {
+			has_resource_changed = true;
+		} else if (meta.size() >= 4) { // Sub-Arrays or Sub-Dictionaries
 			Variant::Type var_type = meta[2];
 			int key_index = meta[3];
 
-			// TODO: do the same for dictionaries too
 			if (var_type == Variant::ARRAY) {
 				Array parent_meta = item->get_parent()->get_metadata(0);
 				Ref<Resource> parent = parent_meta[0];
 				Array parent_resource_array = parent->get(meta[1]);
-				parent_resource_array.set(key_index, unique_resource);
+				Array parent_resource_array_clone = parent_resource_array.duplicate();
+				parent_resource_array_clone.set(key_index, unique_resource);
+				parent->set(meta[1] ,parent_resource_array_clone);
+			}
+			else if (var_type == Variant::DICTIONARY) {
+				bool is_key = meta[4];
+				Array parent_meta = item->get_parent()->get_metadata(0);
+				Ref<Resource> parent = parent_meta[0];
+				Dictionary parent_resource_dictionary = parent->get(meta[1]);
+				Dictionary parent_resource_dictionary_clone = parent_resource_dictionary.duplicate();
+				Variant found_key = parent_resource_dictionary_clone.get_key_at_index(key_index);
+				if (is_key) {
+					Variant found_value = parent_resource_dictionary_clone.get_valid(found_key);
+					parent_resource_dictionary_clone.erase(found_key);
+					parent_resource_dictionary_clone.set(unique_resource, found_value);
+				} else {
+					parent_resource_dictionary_clone.set(found_key, unique_resource);
+				}
+				
 			}
 		}
-		else {
+		else { // Regular properties
 			Array parent_meta = item->get_parent()->get_metadata(0);
 			Ref<Resource> parent = parent_meta[0];
 			parent->set(meta[1], unique_resource);
 		}
 	}
+	if (has_resource_changed) {
+		_resource_changed();
+	}
+	// TODO: we need changes of arrays and dictionaries to also send a signal
 }
 
 EditorResourcePicker::EditorResourcePicker(bool p_hide_assign_button_controls) {
